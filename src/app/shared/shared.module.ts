d18de599cf281f6adf90879e22badef9df2e71da
Dev Palacio <danielpalaciouribe@gmail.com>
2026-03-12 14:26:27 -0500

    feat(layout): improve layout structure with sticky footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
 import { ToastComponent } from './components/toast/toast.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ConfirmModalComponent, ToastComponent],
+  declarations: [
+    HeaderComponent,
+    ConfirmModalComponent,
+    ToastComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, ConfirmModalComponent, ToastComponent],
+  exports: [
+    HeaderComponent,
+    ConfirmModalComponent,
+    ToastComponent,
+    FooterComponent,
+  ],
 })
 export class SharedModule {}
