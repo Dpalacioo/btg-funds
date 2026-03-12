15f986f3d0058f62666d11efcfd7108dbcc8663b
Dev Palacio <danielpalaciouribe@gmail.com>
2026-03-11 19:13:56 -0500

    feat: implement reactive user balance state with RxJS

--- src/app/core/services/balance.service.ts
+++ src/app/core/services/balance.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BalanceService {
+  private balanceSubject = new BehaviorSubject<number>(500000);
 
-  constructor() { }
+  balance$ = this.balanceSubject.asObservable();
+
+  getBalance(): number {
+    return this.balanceSubject.value;
+  }
+
+  updateBalance(newBalance: number) {
+    this.balanceSubject.next(newBalance);
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { BalanceService } from '../../../core/services/balance.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  balance$: Observable<number>;
+
+  constructor(private balanceService: BalanceService) {
+    this.balance$ = this.balanceService.balance$;
+  }
+}
